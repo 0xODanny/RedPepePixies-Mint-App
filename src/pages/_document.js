4b8ec341766aa61b4cbefffc8cc8947ae5eabc5e
0xODanny <166062095+0xODanny@users.noreply.github.com>
2025-04-17 15:03:08 -0700

    Updated site with working mint, success page, and favicon

---
+++ src/pages/_document.js
@@ -0,0 +1,19 @@
+// pages/_document.js
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        {/* Favicon link */}
+        <link rel="icon" href="/favicon.png" />
+        {/* Optional: Default page title */}
+        <title>Balln Raffle</title>
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
